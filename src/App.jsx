13fc650e3fd9febebb312299fcae14e83de397ce
chiaralle <chiaraallegrino@gmail.com>
2025-06-12 17:24:36 +0200

    add Milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,24 @@ import HomePage from "./components/HomePage";
 import Navbar from "./components/Navbar";
 import TaskList from "./components/TaskList";
 import AddTask from "./components/AddTask";
+import GlobalContext from "./components/context/GlobalContext";
+import GlobalContextProvider from "./components/context/GlobalContext";
 
 
 function App() {
 
   return (
-    <BrowserRouter>
+    <GlobalContextProvider>
+      <BrowserRouter>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />}/>
-        <Route path="/TaskList" element={<TaskList />}/>
-        <Route path="AddTask" element={<AddTask />}/>
-      </Routes>
-    </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />}/>
+          <Route path="/TaskList" element={<TaskList />}/>
+          <Route path="AddTask" element={<AddTask />}/>
+        </Routes>
+      </BrowserRouter>
+    </GlobalContextProvider>
+    
   )
 }
 
---
+++ src/components/context/GlobalContext.jsx
@@ -0,0 +1,45 @@
+import { createContext, useEffect, useState } from "react";
+
+
+export const GlobalContext = createContext();
+
+function GlobalContextProvider( {children} ){
+
+
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        /*
+        fetch(import.meta.env.VITE_API_URL)
+            .then(res => res.json())
+            .then(data => {
+                console.log('Dati ricevuti:', data);
+                setTasks(data)})
+            .catch(err => console.error(err))*/
+        const fetchTasks = async () => {
+            try {
+                const response = await fetch(import.meta.env.VITE_API_URL);
+                if (!response.ok) {
+                    throw new Error(`Errore! status: ${response.status}`);
+                }
+                const data = await response.json();
+                console.log('Dati ricevuti:', data);
+                setTasks(data);
+            } catch (error) {
+                console.error('Errore nel recupero delle tasks:', error);
+        }
+        };
+
+        fetchTasks();
+    }, [])
+
+    return (
+        <GlobalContext.Provider value={{ tasks, setTasks }}>
+            {children}
+        </GlobalContext.Provider>
+    )
+}
+
+
+export default GlobalContextProvider;
+
