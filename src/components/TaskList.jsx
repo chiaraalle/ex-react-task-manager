94474307df60e276626d2da3f848acd14203a585
chiaralle <chiaraallegrino@gmail.com>
2025-06-13 16:29:23 +0200

    milestone 3

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,14 +1,12 @@
+import TaskRow from "./TaskRow";
 import { useContext } from "react";
 import { GlobalContext } from "./context/GlobalContext";
 
 function TaskList(){
-
-    const {tasks} = useContext(GlobalContext);
-    console.log('Tasks:', tasks);
+    const { tasks } = useContext(GlobalContext);
 
     return(
         <>
-            <h1>Le tue task</h1>
             <table>
                 <thead>
                     <tr>
@@ -18,36 +16,24 @@ function TaskList(){
                     </tr>
                 </thead>
                 <tbody>
-                    {tasks.map((task, i) => {
-                        const {title, status, createdAt} = task;
-                        return(
-                            <tr key={i}>
-                                <td>{title}</td>
-                                <td>{status}</td>
-                                <td>{createdAt}</td>
-                            </tr>
-                        )
-                    })}
+                    {tasks.map(task => (
+                        <TaskRow 
+                            key={task.id}
+                            task={task}
+                        />
+                    ))}
                 </tbody>
             </table>
         </>
     )
-    
 }
 
 export default TaskList;
 
 /*
-Strutturare TaskList.jsx come una tabella, con le intestazioni Nome, Stato, Data di Creazione.
-
-Creare un componente TaskRow.jsx, che rappresenta una singola riga della tabella e mostra solo le proprietà title, 
-status e createdAt (escludendo description).
-
-Applicare uno stile differente alla colonna status, assegnando i seguenti colori di sfondo alle celle in base 
-al valore dello stato:
+Applicare uno stile differente alla colonna status, assegnando i seguenti colori di sfondo alle celle in base al valore dello stato:
 "To do" → rosso
 "Doing" → giallo
 "Done" → verde
 
-Utilizzare React.memo() su TaskRow.jsx per ottimizzare le prestazioni ed evitare render inutili.
- */
+*/
