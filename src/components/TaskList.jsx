0e9fbaf965ebb502647bf792914cce4b66754103
chiaralle <chiaraallegrino@gmail.com>
2025-06-13 10:32:27 +0200

    add map tasks and table

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -7,11 +7,47 @@ function TaskList(){
     console.log('Tasks:', tasks);
 
     return(
-        <div>
-            <h1>Qui ci andranno le task</h1>
-        </div>
+        <>
+            <h1>Le tue task</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Stato</th>
+                        <th>Data di Creazione</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tasks.map((task, i) => {
+                        const {title, status, createdAt} = task;
+                        return(
+                            <tr key={i}>
+                                <td>{title}</td>
+                                <td>{status}</td>
+                                <td>{createdAt}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </>
     )
     
 }
 
 export default TaskList;
+
+/*
+Strutturare TaskList.jsx come una tabella, con le intestazioni Nome, Stato, Data di Creazione.
+
+Creare un componente TaskRow.jsx, che rappresenta una singola riga della tabella e mostra solo le proprietà title, 
+status e createdAt (escludendo description).
+
+Applicare uno stile differente alla colonna status, assegnando i seguenti colori di sfondo alle celle in base 
+al valore dello stato:
+"To do" → rosso
+"Doing" → giallo
+"Done" → verde
+
+Utilizzare React.memo() su TaskRow.jsx per ottimizzare le prestazioni ed evitare render inutili.
+ */
